affb4fe382dbda052e4e6aeaa173d58044d8469b
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-05-02 21:53:49 -0300

    Realização do Exércio de Função, multiplicando dois parâmetros

---
+++ Exercicio 2-/function.js
@@ -0,0 +1,4 @@
+//Crie uma função que recebe o array de strings 
+//(Vulgo, lugares em que iremos visitar juntos um dia) [“Estados unidos”, “Canadá”, “Inglaterra”, “Reino unido”, “Portugal”, “Japão”, “Noruega”] 
+//como parâmetro, percorra este array e  simplesmente exiba o nome do lugar que está percorrendo no formato “Em nosso thour pelo mundo, começaremos por {lugar}”
+
---
+++ Exercício 3-/function.js
@@ -0,0 +1,3 @@
+//Crie uma função que emite uma mensagem no console com base na nota de um aluno,
+ //se a nota for igual ou maior que 5, a mensagem que deve ser exibida é “Passou”, 
+ //caso contrário deve ser exibida “Reprovou”, a função deve receber a nota do aluno nos parâmetros
