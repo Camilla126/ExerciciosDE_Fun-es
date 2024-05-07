442af1dd22a07739c68f3e15df5ff61c44551c42
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-05-06 22:02:54 -0300

    Exércio Finalizado

--- Exercício 3-/function.js
+++ Exercício 3-/function.js
@@ -1,3 +1,14 @@
 //Crie uma função que emite uma mensagem no console com base na nota de um aluno,
  //se a nota for igual ou maior que 5, a mensagem que deve ser exibida é “Passou”, 
  //caso contrário deve ser exibida “Reprovou”, a função deve receber a nota do aluno nos parâmetros
+
+ const Aluno = (nota)=>{
+if (nota>= 5){
+      console.log('Passou!')
+}else{
+    console.log('Reprovou!')
+}
+  
+ }
+
+const NotaDeAluno= Aluno(5);
