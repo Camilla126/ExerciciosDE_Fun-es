645f6920ab42085f0e95a805d490a57db45d4f1c
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-05-06 21:24:17 -0300

    Finalização do Exercício 100%pronto

--- Exercicio 2-/function.js
+++ Exercicio 2-/function.js
@@ -2,3 +2,13 @@
 //(Vulgo, lugares em que iremos visitar juntos um dia) [“Estados unidos”, “Canadá”, “Inglaterra”, “Reino unido”, “Portugal”, “Japão”, “Noruega”] 
 //como parâmetro, percorra este array e  simplesmente exiba o nome do lugar que está percorrendo no formato “Em nosso thour pelo mundo, começaremos por {lugar}”
 
+
+const tourPeloMundo=(lugares)=> {
+ for (let index = 0; index < lugares.length;index+=1) {
+    console.log(`Em nosso tour pelo mundo, começaremos por ${lugares[index]}.`);
+
+}
+
+ }
+const lugaresParaVisitar = ["Estados Unidos", "Canadá", "Inglaterra", "Reino Unido", "Portugal", "Japão", "Noruega"];
+tourPeloMundo(lugaresParaVisitar);
